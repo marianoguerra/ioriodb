7ebdbd032caad5dd65cda5044d6fab8d717d4267
Mariano Guerra <luismarianoguerra@gmail.com>
2014-10-02 14:42:32 +0200

    refactoring

--- src/iorio_session.erl
+++ src/iorio_session.erl
@@ -10,31 +10,36 @@
 
 -include_lib("jwt/include/jwt.hrl").
 
+get_security_context(Username) ->
+    % TODO: don't try catch
+    % TODO: this is private
+    try
+        {ok, riak_core_security:get_context(Username)}
+    catch error:badarg ->
+        {error, notfound}
+    end.
+
 session_from_parsed_token(BodyRaw) ->
     Body = jsx:decode(BodyRaw),
     Username = proplists:get_value(<<"u">>, Body),
-    % TODO: don't try catch
-    try
-        % TODO: this is private
-        SecurityCtx = riak_core_security:get_context(Username),
-        {ok, {Username, Body, SecurityCtx}}
-    catch error:badarg ->
-        {error, notfound}
+    case get_security_context(Username) of
+        {ok, SecurityCtx} -> {ok, {Username, Body, SecurityCtx}};
+        Error -> Error
     end.
 
 session_from_token(JWTToken, Secret) ->
     case jwt:decode(JWTToken, Secret) of
         {ok, #jwt{body=BodyRaw}} -> session_from_parsed_token(BodyRaw);
-        {error, _Reason}=Error1 -> Error1;
-        {error, Reason, _Jwt2} -> {error, Reason}
+        {error, Reason}          -> {error, Reason};
+        {error, Reason, _Jwt2}   -> {error, Reason}
     end.
 
 from_request(Req, Secret) ->
     case cowboy_req:header(<<"x-session">>, Req) of
         {undefined, R1} -> {error, nosession, R1};
-        {JWTToken, R2} ->
+        {JWTToken, R2}  ->
             case session_from_token(JWTToken, Secret) of
-                {ok, Session} -> {ok, Session, R2};
+                {ok, Session}   -> {ok, Session, R2};
                 {error, Reason} -> {error, Reason, R2}
             end
     end.
@@ -58,17 +63,17 @@ handle_is_authorized(Req, Secret, State, SetSession) ->
 
 % NOTE: for now user can only operate on bucket with his username,
 % except if he is the admin
-is_authorized_for_bucket(nil, _Bucket) -> false;
+is_authorized_for_bucket(nil, _Bucket)         -> false;
 is_authorized_for_bucket(<<"admin">>, _Bucket) -> true;
-is_authorized_for_bucket(Username, Username) -> true;
-is_authorized_for_bucket(_Username, _Bucket) -> false.
+is_authorized_for_bucket(Username, Username)   -> true;
+is_authorized_for_bucket(_Username, _Bucket)   -> false.
 
 % NOTE: we don't still support per stream permissions
 is_authorized_for_stream(Username, Bucket, _Stream) ->
     is_authorized_for_bucket(Username, Bucket).
 
-username_from_session(nil) -> nil;
-username_from_session(undefined) -> nil;
+username_from_session(nil)              -> nil;
+username_from_session(undefined)        -> nil;
 username_from_session({Username, _, _}) -> Username.
 
 % Bucket is the atom all when operating on all buckets
