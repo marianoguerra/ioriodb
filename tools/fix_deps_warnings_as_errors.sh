83cbb5de8a82d937afef421fd6165145d61f42cf
Mariano Guerra <mariano@marianoguerra.org>
2015-02-19 13:51:39 +0000

    fix iorio build with temp riak_core fork until merged

--- tools/fix_deps_warnings_as_errors.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-sed -i 's/, warnings_as_errors//' deps/poolboy/rebar.config
-sed -i 's/warnings_as_errors, //' deps/meck/rebar.config
-sed -i 's/warnings_as_errors, //' deps/riak_core/rebar.config
-
-sed -i 's/, del_source\/2/, del_source\/2, get_context\/1/' deps/riak_core/src/riak_core_security.erl
-
