f67c21beb856c60d8172ccb578781e9ba0b4a770
Mariano Guerra <mariano@marianoguerra.org>
2015-02-21 22:02:12 +0000

    allow specifying bucket in topic using a configurable separator, otherwise use username, if not authenticated use default username

--- src/iorio_mqtt_handler.erl
+++ src/iorio_mqtt_handler.erl
@@ -5,11 +5,17 @@
          disconnect/1, error/2, publish/2, subscribe/2, unsubscribe/2,
          login/2]).
 
--record(state, {username = <<"_user_admin">>, send}).
+-record(state, {username, send, bucket_separator}).
 
 -include_lib("sblob/include/sblob.hrl").
 
-init([{mqttl_send, Send}]) -> {ok, #state{send=Send}}.
+init(Opts) ->
+    {mqttl_send, Send} = proplists:lookup(mqttl_send, Opts),
+    BucketSeparator = proplists:get_value(bucket_separator, Opts, <<"!">>),
+    DefaultUsername = proplists:get_value(default_username, Opts, <<"mqtt">>),
+    {ok, #state{send=Send, bucket_separator=BucketSeparator,
+                username=DefaultUsername}}.
+
 ping(State=#state{}) -> {ok, State}.
 connect(State=#state{}) -> {ok, State}.
 disconnect(State=#state{}) -> {ok, State}.
@@ -18,42 +24,53 @@ error(State=#state{}, Error) ->
     lager:error("Error ~p", [Error]),
     {ok, State}.
 
-publish(State=#state{username=Username}, {Topic, Qos, Dup, Retain, MessageId, Payload}) ->
+publish(State=#state{username=Username, bucket_separator=BucketSeparator},
+        {Topic, Qos, Dup, Retain, MessageId, Payload}) ->
     lager:info("Publish ~p ~p ~p ~p ~p ~p",
                [Topic, Qos, Dup, Retain, MessageId, Payload]),
-    iorio:put(Username, to_binary(Topic), Payload),
+    {Bucket, Stream} = extract_bucket_and_stream(Topic, Username,
+                                                 BucketSeparator),
+    iorio:put(Bucket, Stream, Payload),
     {ok, State}.
 
-subscribe(State=#state{username=Username}, Topics) ->
-    R = lists:foldl(fun ({TopicName, Qos}, {QosList, IState}) ->
-                            OState = IState,
-                            QosVal = Qos,
-                            lager:info("Subscribe ~p ~p", [TopicName, Qos]),
-                            iorio:subscribe(Username, to_binary(TopicName),
-                                            self()),
-                            {[QosVal|QosList], OState}
-                    end, {[], State}, Topics),
+subscribe(State=#state{username=Username, bucket_separator=BucketSeparator},
+          Topics) ->
+    Fun = fun ({TopicName, Qos}, {QosList, IState}) ->
+                  OState = IState,
+                  QosVal = Qos,
+                  {Bucket, Stream} = extract_bucket_and_stream(TopicName,
+                                                               Username,
+                                                               BucketSeparator),
+                  lager:info("Subscribe ~p ~p", [TopicName, Qos]),
+                  iorio:subscribe(Bucket, Stream, self()),
+                  {[QosVal|QosList], OState}
+          end,
+    R = lists:foldl(Fun, {[], State}, Topics),
     {QoSList, NewState} = R,
     {ok, QoSList, NewState}.
 
-unsubscribe(State=#state{username=Username}, Topics) ->
-    NewState = lists:foldl(fun (TopicName, IState) ->
-                            OState = IState,
-                            lager:info("Unsubscribe ~p", [TopicName]),
-                            iorio:unsubscribe(Username, to_binary(TopicName),
-                                              self()),
-                            OState
-                    end, State, Topics),
+unsubscribe(State=#state{username=Username, bucket_separator=BucketSeparator}, Topics) ->
+    Fun = fun (TopicName, IState) ->
+                  OState = IState,
+                  {Bucket, Stream} = extract_bucket_and_stream(TopicName,
+                                                               Username,
+                                                               BucketSeparator),
+                  lager:info("Unsubscribe ~p", [TopicName]),
+                  iorio:unsubscribe(Bucket, Stream, self()),
+                  OState
+          end,
+    NewState = lists:foldl(Fun, State, Topics),
     {ok, NewState}.
 
 timeout(State=#state{}, InactiveMs) ->
     lager:info("session was inactive for ~p ms", [InactiveMs]),
     {ok, State}.
 
-info(State=#state{send=Send}, {entry, _Bucket, Topic,
-                               #sblob_entry{timestamp=_Timestamp,
-                                            seqnum=_SeqNum, data=Data}}) ->
+info(State=#state{send=Send, bucket_separator=BucketSeparator},
+     {entry, Bucket, Stream, #sblob_entry{timestamp=_Timestamp,
+                                          seqnum=_SeqNum, data=Data}}) ->
     % XXX: generate a msg id
+    Topic = topic_from_bucket_and_stream(Bucket, Stream, BucketSeparator),
     MsgId = 1,
     Send(MsgId, Topic, Data),
     {ok, State};
@@ -76,3 +93,16 @@ login(State=#state{}, {Username, Password}) ->
 
 to_binary(Val) when is_binary(Val) -> Val;
 to_binary(Val) when is_list(Val) -> list_to_binary(Val).
+
+extract_bucket_and_stream(Topic, Username, Sep) when is_list(Topic) ->
+    BTopic = list_to_binary(Topic),
+    extract_bucket_and_stream(BTopic, Username, Sep);
+extract_bucket_and_stream(Topic, Username, Sep) ->
+    case binary:split(Topic, [Sep]) of
+        [Stream] -> {Username, Stream};
+        [Bucket, Stream] -> {Bucket, Stream};
+        [Bucket|Tail] -> {Bucket, list_to_binary(Tail)}
+    end.
+
+topic_from_bucket_and_stream(Bucket, Stream, Sep) ->
+    list_to_binary([Bucket, Sep, Stream]).
