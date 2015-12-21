c339fdab5cc94fb135b6bfc39d1248f7ee2791f2
Mariano Guerra <mariano@marianoguerra.org>
2015-12-21 17:35:46 +0000

    only shutdown if active, otherwise return empty list

--- src/iorio_listen_handler.erl
+++ src/iorio_listen_handler.erl
@@ -41,9 +41,13 @@ info({smc, {heartbeat, Props}}, Req, State=#state{channels=Channels}) ->
        true -> ok
     end,
     {ok, Req, State};
-info({smc, {terminate, Info}}, Req, State) ->
+info({smc, {terminate, Info}}, Req, State=#state{active=Active}) ->
     lager:info("channel terminated ~p, shuting down listen", [Info]),
-    {shutdown, Req, State};
+    if Active ->
+           {shutdown, Req, State};
+       true ->
+           reply_entries_json([], Req, State)
+    end;
 info({smc, Info}, Req, State=#state{channels=Channels}) ->
     lager:info("channel update for ~p: ~p (~p)", [self(), Info, Channels]),
     {ok, Req, State};
